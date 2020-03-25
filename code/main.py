3a138b95312d3ac555108736210e852355621e23
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-03-25 20:18:23 +0530

    fixed bug

--- code/main.py
+++ code/main.py
@@ -120,9 +120,9 @@ def main():
         #     except WorkspaceException as exception:
         #         print(f"::error::Creating new Workspace failed: {exception}")
         #         raise AMLConfigurationException(f"Creating new Workspace failed with 'WorkspaceException': {exception}.")
-        else:
-            print(f"::error::Loading existing Workspace failed with 'WorkspaceException' and new Workspace will not be created because parameter 'createWorkspace' was not defined or set to false in your parameter file: {exception}")
-            raise AMLConfigurationException("Loading existing Workspace failed with 'WorkspaceException' and new Workspace will not be created because parameter 'createWorkspace' was not defined or set to false in your parameter file.")
+        # else:
+        #     print(f"::error::Loading existing Workspace failed with 'WorkspaceException' and new Workspace will not be created because parameter 'createWorkspace' was not defined or set to false in your parameter file: {exception}")
+        #     raise AMLConfigurationException("Loading existing Workspace failed with 'WorkspaceException' and new Workspace will not be created because parameter 'createWorkspace' was not defined or set to false in your parameter file.")
 
     # # Write Workspace ARM properties to config file
     # print("::debug::Writing Workspace ARM properties to config file")
