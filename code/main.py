8f378178e60227af23882eede89dabc5225eec0a
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-03-25 20:14:13 +0530

    check exception raised

--- code/main.py
+++ code/main.py
@@ -81,50 +81,48 @@ def main():
         )
         print("::debug::Successfully loaded existing Workspace")
         print(ws)
-    except:
-        print(" some exception occured")
-    # except AuthenticationException as exception:
-    #     print(f"::error::Could not retrieve user token. Please paste output of `az ad sp create-for-rbac --name <your-sp-name> --role contributor --scopes /subscriptions/<your-subscriptionId>/resourceGroups/<your-rg> --sdk-auth` as value of secret variable: AZURE_CREDENTIALS: {exception}")
-    #     raise AuthenticationException
-    # except AuthenticationError as exception:
-    #     print(f"::error::Microsoft REST Authentication Error: {exception}")
-    #     raise AuthenticationException
-    # except AdalError as exception:
-    #     print(f"::error::Active Directory Authentication Library Error: {exception}")
-    #     raise AdalError
-    # except ProjectSystemException as exception:
-    #     print(f"::error::Workspace authorizationfailed: {exception}")
-    #     raise ProjectSystemException
-    # except WorkspaceException as exception:
-    #     print(f"::debug::Loading existing Workspace failed: {exception}")
-    #     if parameters.get("create_workspace", False):
-    #         try:
-    #             print("::debug::Creating new Workspace")
-    #             ws = Workspace.create(
-    #                 name=parameters.get("name", None),
-    #                 subscription_id=azure_credentials.get("subscriptionId", ""),
-    #                 resource_group=parameters.get("resource_group", None),
-    #                 location=parameters.get("location", None),
-    #                 create_resource_group=parameters.get("create_resource_group", False),
-    #                 sku=parameters.get("sku", "basic"),
-    #                 friendly_name=parameters.get("friendly_name", None),
-    #                 storage_account=parameters.get("storage_account", None),
-    #                 key_vault=parameters.get("key_vault", None),
-    #                 app_insights=parameters.get("app_insights", None),
-    #                 container_registry=parameters.get("container_registry", None),
-    #                 cmk_keyvault=parameters.get("cmk_key_vault", None),
-    #                 resource_cmk_uri=parameters.get("resource_cmk_uri", None),
-    #                 hbi_workspace=parameters.get("hbi_workspace", None),
-    #                 auth=sp_auth,
-    #                 exist_ok=True,
-    #                 show_output=True
-    #             )
-    #         except WorkspaceException as exception:
-    #             print(f"::error::Creating new Workspace failed: {exception}")
-    #             raise AMLConfigurationException(f"Creating new Workspace failed with 'WorkspaceException': {exception}.")
-    #     else:
-    #         print(f"::error::Loading existing Workspace failed with 'WorkspaceException' and new Workspace will not be created because parameter 'createWorkspace' was not defined or set to false in your parameter file: {exception}")
-    #         raise AMLConfigurationException("Loading existing Workspace failed with 'WorkspaceException' and new Workspace will not be created because parameter 'createWorkspace' was not defined or set to false in your parameter file.")
+    except AuthenticationException as exception:
+        print(f"::error::Could not retrieve user token. Please paste output of `az ad sp create-for-rbac --name <your-sp-name> --role contributor --scopes /subscriptions/<your-subscriptionId>/resourceGroups/<your-rg> --sdk-auth` as value of secret variable: AZURE_CREDENTIALS: {exception}")
+        raise AuthenticationException
+    except AuthenticationError as exception:
+        print(f"::error::Microsoft REST Authentication Error: {exception}")
+        raise AuthenticationException
+    except AdalError as exception:
+        print(f"::error::Active Directory Authentication Library Error: {exception}")
+        raise AdalError
+    except ProjectSystemException as exception:
+        print(f"::error::Workspace authorizationfailed: {exception}")
+        raise ProjectSystemException
+    except WorkspaceException as exception:
+        print(f"::debug::Loading existing Workspace failed: {exception}")
+        # if parameters.get("create_workspace", False):
+        #     try:
+        #         print("::debug::Creating new Workspace")
+        #         ws = Workspace.create(
+        #             name=parameters.get("name", None),
+        #             subscription_id=azure_credentials.get("subscriptionId", ""),
+        #             resource_group=parameters.get("resource_group", None),
+        #             location=parameters.get("location", None),
+        #             create_resource_group=parameters.get("create_resource_group", False),
+        #             sku=parameters.get("sku", "basic"),
+        #             friendly_name=parameters.get("friendly_name", None),
+        #             storage_account=parameters.get("storage_account", None),
+        #             key_vault=parameters.get("key_vault", None),
+        #             app_insights=parameters.get("app_insights", None),
+        #             container_registry=parameters.get("container_registry", None),
+        #             cmk_keyvault=parameters.get("cmk_key_vault", None),
+        #             resource_cmk_uri=parameters.get("resource_cmk_uri", None),
+        #             hbi_workspace=parameters.get("hbi_workspace", None),
+        #             auth=sp_auth,
+        #             exist_ok=True,
+        #             show_output=True
+        #         )
+        #     except WorkspaceException as exception:
+        #         print(f"::error::Creating new Workspace failed: {exception}")
+        #         raise AMLConfigurationException(f"Creating new Workspace failed with 'WorkspaceException': {exception}.")
+        else:
+            print(f"::error::Loading existing Workspace failed with 'WorkspaceException' and new Workspace will not be created because parameter 'createWorkspace' was not defined or set to false in your parameter file: {exception}")
+            raise AMLConfigurationException("Loading existing Workspace failed with 'WorkspaceException' and new Workspace will not be created because parameter 'createWorkspace' was not defined or set to false in your parameter file.")
 
     # # Write Workspace ARM properties to config file
     # print("::debug::Writing Workspace ARM properties to config file")
