9d1711c7d1fedc7b7b9686227f0ffb8df98a2f12
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-03-27 12:42:51 +0530

    correcting yamls

--- code/computeManager.py
+++ code/computeManager.py
@@ -17,7 +17,7 @@ class ComputeTargetManager(object):
     
     def executeAction(self, parameters_file =None, ws = None, azure_credentials = None , azure_computeTarget = None):
         try:
-        azure_credentials = json.loads(azure_credentials)
+            azure_credentials = json.loads(azure_credentials)
         except JSONDecodeError:
             print("::error::Please paste output of `az ad sp create-for-rbac --name <your-sp-name> --role contributor --scopes /subscriptions/<your-subscriptionId>/resourceGroups/<your-rg> --sdk-auth` as value of secret variable: AZURE_CREDENTIALS")
             raise AMLConfigurationException(f"Incorrect or poorly formed output from azure credentials saved in AZURE_CREDENTIALS secret. See setup in https://github.com/Azure/aml-compute/blob/master/README.md")
